48d88e298134064e4ebdbb7dd5224c395bcbf293
Mohammed Faizuddin <faiz@avantari.org>
2018-10-29 20:12:55 +0530

    initial commit flow-app-component-container

---
+++ src/index.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import AppComponent from 'flow-app-component';
+
+import './css/theme/default.css';
+
+class ContainerComponent extends AppComponent {
+  constructor() {
+    super();
+    const newState = {
+      properties: [
+        {
+          categoryName: 'General',
+          categoryDescription: 'Basic settings for the container',
+          properties: [],
+        },
+        {
+          categoryName: 'Events',
+          categoryDescription: 'Events for the container',
+          properties: [
+            {
+              id: 'event',
+              name: 'Events',
+              type: 'graph',
+              options: {},
+              data: null,
+            },
+          ],
+        },
+      ],
+      iconUrl: '/assets/images/container-component.png',
+      name: 'Container',
+      type: 'ui-component',
+      componentType: 'container',
+      category: 'Wrappers',
+      parent: null,
+      showOnComponentsPanel: true,
+      isValuable: true,
+      allowsChildren: true,
+    };
+
+    this.state = Object.assign(this.state, newState); // merge two states together, and dont lose any parent state properties.
+  }
+
+  renderContent() {
+    let customStyles = {};
+    if (!this.props.hasChildren) {
+      customStyles = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100px',
+      };
+    }
+    return (
+      <div className="ui-container" style={customStyles}>
+        {!this.props.hasChildren && <span>Container</span>}
+        {this.renderChildren()}
+      </div>
+    );
+  }
+}
+
+export default ContainerComponent;

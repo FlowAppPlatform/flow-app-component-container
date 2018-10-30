dd3fe56573aba6e52f83bf9516262a359c5ea4c5
Mohammed Faizuddin <faiz@avantari.org>
2018-10-30 19:33:50 +0530

    update flow-app-component to 1.1.0

--- src/index.js
+++ src/index.js
@@ -5,18 +5,6 @@ import AppComponent from 'flow-app-component';
 import './css/theme/default.css';
 
 class ContainerComponent extends AppComponent {
-  static properties = {
-    iconUrl: '/assets/images/container-component.png',
-    name: 'Container',
-    type: 'ui-component',
-    componentType: 'container',
-    category: 'Wrappers',
-    parent: null,
-    showOnComponentsPanel: true,
-    isValuable: true,
-    allowsChildren: true
-  };
-
   constructor() {
     super();
     const newState = {
@@ -40,8 +28,15 @@ class ContainerComponent extends AppComponent {
           ],
         },
       ],
-
-      ...ContainerComponent.properties
+      iconUrl: '/assets/images/container-component.png',
+      name: 'Container',
+      type: 'ui-component',
+      componentType: 'container',
+      category: 'Wrappers',
+      parent: null,
+      showOnComponentsPanel: true,
+      isValuable: true,
+      allowsChildren: true,
     };
 
     this.state = Object.assign(this.state, newState); // merge two states together, and dont lose any parent state properties.
